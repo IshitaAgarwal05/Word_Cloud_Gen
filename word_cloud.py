0525d0e532a6d3f78e76a5a120ab7dfb89c9f127
Ishita Agarwal <111743570+IshitaAgarwal05@users.noreply.github.com>
2024-08-05 05:36:33 -0400

    Added comments

--- word_cloud.py
+++ word_cloud.py
@@ -1,9 +1,14 @@
+# Import the libraries
 from wordcloud import WordCloud, STOPWORDS
 
+# Take text input to be put into the cloud image
 text = input("Enter your text: ")
 stopwords = set(STOPWORDS)
+
+# Set font for the text in the image
 font_path = "fonts/radiant-kingdom-font/RadiantKingdom-mL5eV.ttf"
 
+# Set wordcloud attributes and parameters
 wordcloud = WordCloud(width = 800, height = 800,
                       background_color="black",
                       contour_color="skyblue",
@@ -15,6 +20,7 @@ wordcloud = WordCloud(width = 800, height = 800,
                       max_font_size = 100,
                       font_path=font_path).generate(text)
 
+# Convert, show, and save the image as a png file in your PC 
 img = wordcloud.to_image()
 img.show()
 
